66a0e8937fbb0c80beb71e94985ddf69cca26d07
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-08-25 12:51:29 -0700

    Create mock of optipng to validate %PATH% scenario

--- tool/tests/OptiPNG.Runner.Tests/CliRunnerTests.cs
+++ tool/tests/OptiPNG.Runner.Tests/CliRunnerTests.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 using CliWrap;
 using CliWrap.Buffered;
 
@@ -5,20 +7,70 @@ namespace OptiPNG.Runner.Tests;
 
 public class When_OptiPNG_is_discoverable
 {
+    private readonly record struct PlatformInfo(string RuntimeIdentifier, string EnvPathSeparator);
+
+    private readonly ITestOutputHelper _output;
+
+    public When_OptiPNG_is_discoverable(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    private static PlatformInfo GetPlatformInfo()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            return new PlatformInfo
+            {
+                RuntimeIdentifier = "win-x64",
+                EnvPathSeparator = ";"
+            };
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            return new PlatformInfo
+            {
+                RuntimeIdentifier = "linux-x64",
+                EnvPathSeparator = ":"
+            };
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            return new PlatformInfo
+            {
+                RuntimeIdentifier = "osx-x64",
+                EnvPathSeparator = ":"
+            };
+        }
+        else
+        {
+            throw new PlatformNotSupportedException($"Platform with Runtime Identifier {Environment.OSVersion} not supported");
+        }
+    }
+
+    private string BuildPathToOptiPNGMock(string runtimeIdentifier)
+    {
+        var path = Path.Combine(Assembly.GetEntryAssembly()?.Location ?? string.Empty, "..", "..", "..", "..", "..", "OptiPNG.Mock", "bin", "Debug", "net7.0", runtimeIdentifier, "publish");
+        return Path.GetFullPath(path);
+    }
+
     [Fact]
     public async Task It_run_and_returns_a_zero_exit_code()
     {
+        var platformInfo = GetPlatformInfo();
+        var mockPath = BuildPathToOptiPNGMock(platformInfo.RuntimeIdentifier);
+        _output.WriteLine("Using mock path `{0}`", mockPath);
+
         var result = await Cli.Wrap("OptiPNGRunner")
             .WithEnvironmentVariables(new Dictionary<string, string?>
             {
-                // TODO: Make this test portable
-                { "PATH", $@"{Environment.GetEnvironmentVariable("PATH")};C:\optipng" }
+                { "PATH", $@"{Environment.GetEnvironmentVariable("PATH")}{platformInfo.EnvPathSeparator}{mockPath}" }
             })
             .ExecuteBufferedAsync();
 
         result.ExitCode.Should().Be(0);
 
-        result.StandardOutput.Should().NotBeEmpty();
+        result.StandardOutput.Trim().Should().Be("Hello, World!");
         result.StandardError.Should().BeEmpty();
     }
 }
