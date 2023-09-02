b6fbd9ca280a91c152554587476542ea0a919671
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-09-02 14:59:33 -0700

    Add OptiPNG.Wrapper tool that checks for optimization

---
+++ tool/src/OptiPNG.Wrapper/Program.cs
@@ -0,0 +1,30 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+namespace OptiPNG.Wrapper;
+
+internal class Program
+{
+    private static async Task<int> Main(string[] args)
+    {
+        var files = args ?? Array.Empty<string>();
+
+        var result = await Cli.Wrap("OptiPNGRunner")
+            .WithArguments(new[] { "-simulate" }.Union(files))
+            .WithValidation(CommandResultValidation.None)
+            .ExecuteBufferedAsync();
+
+        var exitCode = result.ExitCode;
+
+        foreach (var file in files)
+        {
+            if (!result.StandardError.Contains($"{file} is already optimized."))
+            {
+                Console.Error.WriteLine($"'{file}' is not optimized. Run optipng to optimize and try again.");
+                exitCode = -1;
+            }
+        }
+
+        return exitCode;
+    }
+}
