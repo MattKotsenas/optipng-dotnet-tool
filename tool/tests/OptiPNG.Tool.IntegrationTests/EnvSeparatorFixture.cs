22112f53c7216e5e52c8bb2714454e05d478017c
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-10-13 23:05:37 -0700

    Migrate custom vendor lib test to package test and delete direct tool tests

---
+++ tool/tests/OptiPNG.Tool.IntegrationTests/EnvSeparatorFixture.cs
@@ -0,0 +1,14 @@
+﻿using OptiPNG.Launcher;
+
+namespace OptiPNG.Tool.IntegrationTests;
+
+public class EnvSeparatorFixture
+{
+    public string Separator { get; init; }
+
+    public EnvSeparatorFixture()
+    {
+        var inspector = new PlatformInspector();
+        Separator = inspector.Inspect().EnvPathSeparator;
+    }
+}
--- tool/tests/OptiPNG.Tool.IntegrationTests/ToolInstallTests.cs
+++ tool/tests/OptiPNG.Tool.IntegrationTests/ToolInstallTests.cs
@@ -4,46 +4,29 @@ using System.Reflection;
 using CliWrap;
 using CliWrap.Buffered;
 
-using OptiPNG.Launcher;
-
 namespace OptiPNG.Tool.IntegrationTests;
 
-public class When_running_the_default_tool
+public class When_running_the_tool_with_a_custom_lib : IClassFixture<NuGetConfigFixture>, IClassFixture<EnvSeparatorFixture>
 {
     private readonly ITestOutputHelper _output;
     private readonly IFileSystem _fs = new FileSystem();
     private readonly string _nugetConfig;
+    private readonly string _envSeparator;
 
-    public When_running_the_default_tool(ITestOutputHelper output)
+    public When_running_the_tool_with_a_custom_lib(NuGetConfigFixture nugetFixture, EnvSeparatorFixture separatorFixture, ITestOutputHelper output)
     {
         _output = output;
-        _nugetConfig = GenerateNuGetConfig();
+        _nugetConfig = nugetFixture.NuGetConfig;
+        _envSeparator = separatorFixture.Separator;
+
+        _output.WriteLine($"Using NuGet package path '{nugetFixture.PackagePath}'");
     }
 
-    private string GenerateNuGetConfig()
+    private string BuildPathToOptiPNGMock()
     {
-        // Pull the artifacts package path from the <AssemblyMetadata> MSBuild property in the project file
-        IEnumerable<AssemblyMetadataAttribute> attributes = typeof(When_running_the_default_tool).Assembly.GetCustomAttributes<AssemblyMetadataAttribute>();
-        string? packagePath = attributes.Single(attribute => attribute.Key == "PackagePath").Value;
-
-        ArgumentNullException.ThrowIfNull(packagePath);
-        _output.WriteLine($"Using NuGet package path '{packagePath}'");
-
-        return
-@$"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <packageSources>
-    <clear />
-    <add key=""Local"" value=""{packagePath}"" />
-  </packageSources>
-</configuration>
-";
-    }
-
-    private static string GetEnvSeparator()
-    {
-        var inspector = new PlatformInspector();
-        return inspector.Inspect().EnvPathSeparator;
+        // TODO: Make this better, perhaps with MSBuild TargetOutputs -- https://learn.microsoft.com/en-us/visualstudio/msbuild/msbuild-task?view=vs-2022
+        var path = Path.Combine(Assembly.GetEntryAssembly()?.Location ?? string.Empty, "..", "..", "..", "..", "..", "OptiPNG.Mock", "bin", "Debug", "net7.0", "publish");
+        return Path.GetFullPath(path);
     }
 
     private static async Task<BufferedCommandResult> Install(string temp, string nuget)
@@ -55,13 +38,71 @@ public class When_running_the_default_tool
         .ExecuteBufferedAsync();
     }
 
-    private static async Task<BufferedCommandResult> Run(string temp)
+    private async Task<BufferedCommandResult> Run(string temp)
+    {
+        var mockPath = BuildPathToOptiPNGMock();
+        _output.WriteLine("Using mock path `{0}`", mockPath);
+
+        return await Cli.Wrap("dotnet")
+            .WithArguments("optipng")
+            .WithEnvironmentVariables(new Dictionary<string, string?>
+            {
+                ["PATH"] = Environment.GetEnvironmentVariable("PATH") + $"{_envSeparator}{temp}{_envSeparator}{mockPath}"
+            })
+            .ExecuteBufferedAsync();
+    }
+
+    [Fact]
+    public async Task The_custom_lib_is_used()
+    {
+        using (_fs.CreateDisposableDirectory(out IDirectoryInfo temp))
+        {
+            _output.WriteLine($"Using temp directory '{temp.FullName}'");
+
+            string nugetConfigPath = Path.Combine(temp.FullName, "nuget.config");
+
+            _fs.File.WriteAllText(nugetConfigPath, _nugetConfig);
+
+            await Install(temp.FullName, nugetConfigPath);
+            BufferedCommandResult result = await Run(temp.FullName);
+
+            result.StandardOutput.Trim().Should().Be("Hello, World!");
+        }
+    }
+}
+
+public class When_running_the_default_tool : IClassFixture<NuGetConfigFixture>, IClassFixture<EnvSeparatorFixture>
+{
+    private readonly ITestOutputHelper _output;
+    private readonly IFileSystem _fs = new FileSystem();
+    private readonly string _nugetConfig;
+    private readonly string _envSeparator;
+
+    public When_running_the_default_tool(NuGetConfigFixture nugetFixture, EnvSeparatorFixture separatorFixture, ITestOutputHelper output)
+    {
+        _output = output;
+        _nugetConfig = nugetFixture.NuGetConfig;
+        _envSeparator = separatorFixture.Separator;
+
+        _output.WriteLine($"Using NuGet package path '{nugetFixture.PackagePath}'");
+    }
+
+    private static async Task<BufferedCommandResult> Install(string temp, string nuget)
+    {
+        IReadOnlyCollection<string> args = $"tool install optipng.tool --tool-path {temp} --prerelease --configfile {nuget}".Split(" ");
+
+        return await Cli.Wrap("dotnet")
+        .WithArguments(args)
+        .ExecuteBufferedAsync();
+    }
+
+    private async Task<BufferedCommandResult> Run(string temp)
     {
         return await Cli.Wrap("dotnet")
             .WithArguments("optipng")
             .WithEnvironmentVariables(new Dictionary<string, string?>
             {
-                ["PATH"] = Environment.GetEnvironmentVariable("PATH") + $"{GetEnvSeparator()}{temp}"
+                ["PATH"] = Environment.GetEnvironmentVariable("PATH") + $"{_envSeparator}{temp}"
             })
             .ExecuteBufferedAsync();
     }
