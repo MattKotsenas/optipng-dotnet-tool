64d5ed6122d7574d433a2b7c7e8055aeabd0720b
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-11-03 15:08:31 -0700

    Add tests for mixing implicit and explicit

--- msbuild/tests/OptiPNG.MSBuild.IntegrationTests/UnitTest1.cs
+++ msbuild/tests/OptiPNG.MSBuild.IntegrationTests/UnitTest1.cs
@@ -119,3 +119,105 @@ public class Given_a_project_with_an_explicit_reference_to_a_PNG_file : Integrat
         message!.Trim().Should().Be($"Error optimizing '{fileName}': Can't open the input file");
     }
 }
+
+public class Given_a_project_with_both_PNG_files_that_match_the_targets_wildcard_and_an_explicit_reference_to_a_PNG_file : IntegrationTestBase
+{
+    [Fact]
+    public async Task The_explicit_defintion_of_the_ItemGroup_wins_and_an_unoptimized_file_fails_the_build()
+    {
+        using IntegrationTestContext context = new();
+
+        string projectDir = new FileInfo(context.ProjectCreator.FullPath).Directory!.FullName;
+        await CopyEmbeddedResourceToFileAsync($"{GetType().Namespace}.Resources.optimized.png", Path.Join(projectDir, "optimized.png"));
+        await CopyEmbeddedResourceToFileAsync($"{GetType().Namespace}.Resources.unoptimized.png", Path.Join(projectDir, "unoptimized.png"));
+
+        context.ProjectCreator
+            .ItemInclude("PngFiles", @"unoptimized.png")
+            .Save()
+            .TryBuild(restore: true, out bool result, out BuildOutput buildOutput);
+
+        result.Should().BeFalse();
+
+        buildOutput.ErrorEvents.Should().HaveCount(1);
+
+        string? message = buildOutput.ErrorEvents.Single().Message;
+        message.Should().NotBeNull();
+        message!.Trim().Should().EndWith("is not optimized. Run optipng to optimize and try again.");
+    }
+
+    [Fact]
+    public async Task The_explicit_defintion_of_the_ItemGroup_wins_and_an_optimized_file_build_succeeds()
+    {
+        using IntegrationTestContext context = new();
+
+        string projectDir = new FileInfo(context.ProjectCreator.FullPath).Directory!.FullName;
+        await CopyEmbeddedResourceToFileAsync($"{GetType().Namespace}.Resources.optimized.png", Path.Join(projectDir, "optimized.png"));
+        await CopyEmbeddedResourceToFileAsync($"{GetType().Namespace}.Resources.unoptimized.png", Path.Join(projectDir, "unoptimized.png"));
+
+        context.ProjectCreator
+            .ItemInclude("PngFiles", @"optimized.png")
+            .Save()
+            .TryBuild(restore: true, out bool result, out BuildOutput buildOutput);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task When_the_file_is_optimized_the_build_succeeds()
+    {
+        using IntegrationTestContext context = new();
+
+        string projectDir = new FileInfo(context.ProjectCreator.FullPath).Directory!.FullName;
+        await CopyEmbeddedResourceToFileAsync($"{GetType().Namespace}.Resources.optimized.png", Path.Join(projectDir, "optimized.png"));
+
+        context.ProjectCreator
+            .ItemInclude("PngFiles", @"optimized.png")
+            .Save()
+            .TryBuild(restore: true, out bool result, out BuildOutput buildOutput);
+
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task When_the_file_is_not_optimized_the_build_fails()
+    {
+        using IntegrationTestContext context = new();
+
+        string projectDir = new FileInfo(context.ProjectCreator.FullPath).Directory!.FullName;
+        await CopyEmbeddedResourceToFileAsync($"{GetType().Namespace}.Resources.unoptimized.png", Path.Join(projectDir, "unoptimized.png"));
+
+        context.ProjectCreator
+            .ItemInclude("PngFiles", @"unoptimized.png")
+            .Save()
+            .TryBuild(restore: true, out bool result, out BuildOutput buildOutput);
+
+        result.Should().BeFalse();
+
+        buildOutput.ErrorEvents.Should().HaveCount(1);
+
+        string? message = buildOutput.ErrorEvents.Single().Message;
+        message.Should().NotBeNull();
+        message!.Trim().Should().EndWith("is not optimized. Run optipng to optimize and try again.");
+    }
+
+    [Fact]
+    public void When_the_file_does_not_exist_the_build_fails()
+    {
+        using IntegrationTestContext context = new();
+
+        string fileName = @"C:\file that should not exist.png";
+
+        context.ProjectCreator
+            .ItemInclude("PngFiles", fileName)
+            .Save()
+            .TryBuild(restore: true, out bool result, out BuildOutput buildOutput);
+
+        result.Should().BeFalse();
+
+        buildOutput.ErrorEvents.Should().HaveCount(1);
+
+        string? message = buildOutput.ErrorEvents.Single().Message;
+        message.Should().NotBeNull();
+        message!.Trim().Should().Be($"Error optimizing '{fileName}': Can't open the input file");
+    }
+}
