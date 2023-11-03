2a1de52e73f1e47e8ee04b9f0a90ec572dcf0abb
Matt Kotsenas <matt.kotsenas@gmail.com>
2023-11-03 14:02:25 -0700

    Fix misleading error message in Verifier when file not found
    Previously, when a file was not found by the Verifier, it said the file
    wasn't optimized. While that's technically true, it is also misleading.
    
    Instead, split the output into chunks and parse each chunk to determine
    the outcome of each file and display an appropriate error message.

--- msbuild/src/OptiPNG.Verifier/OutputParser.cs
+++ msbuild/src/OptiPNG.Verifier/OutputParser.cs
@@ -1,21 +1,77 @@
-﻿namespace OptiPNG.Verifier;
+﻿using System.Text.RegularExpressions;
+
+namespace OptiPNG.Verifier;
 
 public class OutputParser
 {
+    private static readonly Regex RecordDelimiter = new Regex(@"^\*\* ", RegexOptions.Multiline);
+    private static readonly string[] NewLines = new[] { Environment.NewLine };
+
+    public string NotOptimizedErrorMessageFormatter(string file)
+    {
+        return $"'{file}' is not optimized. Run optipng to optimize and try again.";
+    }
+
+    public string UnknownErrorMessageFormatter(string file, string message)
+    {
+        return $"Error optimizing '{file}': {message}";
+    }
+
     public ParseResult Parse(IReadOnlyCollection<string> files, string output)
     {
         List<string> outputs = new(files.Count);
         bool success = true;
 
-        foreach (var file in files)
+        IReadOnlyCollection<ParseRecord> records = SplitOutputIntoRecords(output);
+
+        foreach (string file in files)
         {
-            if (!output.Contains($"{file} is already optimized."))
+            ParseRecord? record = records.SingleOrDefault(r => r.Header == $"Processing: {file}");
+
+            if (record is null)
             {
-                outputs.Add($"'{file}' is not optimized. Run optipng to optimize and try again.");
+                throw new InvalidDataException($"Unable to parse output for file '{file}'");
+            }
+
+            if (record.Body.Contains("No output: simulation mode."))
+            {
                 success = false;
+                outputs.Add(NotOptimizedErrorMessageFormatter(file));
+            }
+            else if (record.Body[0].StartsWith("Error:"))
+            {
+                success = false;
+
+                string message = record.Body[0].Replace("Error: ", string.Empty);
+                outputs.Add(UnknownErrorMessageFormatter(file, message));
+            }
+            else if (record.Body.Contains($"{file} is already optimized."))
+            {
+                // Happy path; do nothing.
+            }
+            else
+            {
+                throw new InvalidDataException($"Unexpected {nameof(ParseRecord)} format. Data is: '{string.Join(Environment.NewLine, record.Header, record.Body)}'");
             }
         }
 
         return new ParseResult(success, outputs);
     }
+
+    private IReadOnlyCollection<ParseRecord> SplitOutputIntoRecords(string output)
+    {
+        List<ParseRecord> results = new();
+        string[] records = RecordDelimiter.Split(output).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+        foreach (string record in records)
+        {
+            string[] lines = record.Split(NewLines, StringSplitOptions.None);
+
+            results.Add(new ParseRecord(lines[0], lines.Skip(1).ToArray()));
+        }
+
+        return results;
+    }
+
+    private record ParseRecord(string Header, IReadOnlyList<string> Body);
 }
--- msbuild/tests/OptiPNG.Verifier.UnitTests/OutputParserTests.cs
+++ msbuild/tests/OptiPNG.Verifier.UnitTests/OutputParserTests.cs
@@ -25,7 +25,6 @@ Trying:
 
 ";
 
-
     [Fact]
     public void The_OutputParser_returns_success_and_no_output()
     {
@@ -65,6 +64,30 @@ No output: simulation mode.
 
 ";
 
+    [Fact]
+    public void The_OutputParser_returns_failure_and_outputs_the_file_name()
+    {
+        ParseResult result = _outputParser.Parse(new[] { _fileName }, _output);
+
+        result.Failed.Should().BeTrue();
+
+        result.Output.Count.Should().Be(1);
+        result.Output.Single().Should().Be(_outputParser.NotOptimizedErrorMessageFormatter(_fileName));
+    }
+}
+
+public class Given_a_file_that_does_not_exist
+{
+    private readonly OutputParser _outputParser = new();
+    private const string _fileName = @"asdf";
+    private const string _output =
+@$"** Processing: {_fileName}
+Error: Can't open the input file
+
+** Status report
+1 file(s) have been processed.
+1 error(s) have been encountered.
+";
 
     [Fact]
     public void The_OutputParser_returns_failure_and_outputs_the_file_name()
@@ -74,6 +97,72 @@ No output: simulation mode.
         result.Failed.Should().BeTrue();
 
         result.Output.Count.Should().Be(1);
-        result.Output.Single().Should().StartWith($"'{_fileName}' is not optimized");
+        result.Output.Single().Should().Be(_outputParser.UnknownErrorMessageFormatter(_fileName, "Can't open the input file"));
+    }
+}
+
+public class Given_multiple_results_with_one_file_optimized_one_file_unoptimized_and_one_file_that_does_not_exist
+{
+    private readonly OutputParser _outputParser = new();
+    private const string _output =
+@$"** Processing: C:\file that does not exit.png
+Error: Can't open the input file
+
+** Processing: C:\unoptimized.png
+400x250 pixels, 4x8 bits/pixel, RGB+alpha
+Reducing image to 3x8 bits/pixel, RGB
+Input IDAT size = 204268 bytes
+Input file size = 207071 bytes
+
+Trying:
+  zc = 9  zm = 8  zs = 0  f = 0
+  zc = 9  zm = 8  zs = 1  f = 0
+  zc = 1  zm = 8  zs = 2  f = 0
+  zc = 9  zm = 8  zs = 3  f = 0
+  zc = 9  zm = 8  zs = 0  f = 5		IDAT size = 170020
+  zc = 9  zm = 8  zs = 1  f = 5		IDAT size = 161908
+  zc = 1  zm = 8  zs = 2  f = 5
+  zc = 9  zm = 8  zs = 3  f = 5
+                               
+
+Selecting parameters:
+  zc = 9  zm = 8  zs = 1  f = 5		IDAT size = 161908
+
+No output: simulation mode.
+
+** Processing: C:\optimized.png
+400x250 pixels, 3x8 bits/pixel, RGB
+Input IDAT size = 161908 bytes
+Input file size = 164675 bytes
+
+Trying:
+  zc = 9  zm = 8  zs = 0  f = 0
+  zc = 9  zm = 8  zs = 1  f = 0
+  zc = 1  zm = 8  zs = 2  f = 0
+  zc = 9  zm = 8  zs = 3  f = 0
+  zc = 9  zm = 8  zs = 0  f = 5
+  zc = 9  zm = 8  zs = 1  f = 5		IDAT size = 161908
+  zc = 1  zm = 8  zs = 2  f = 5
+  zc = 9  zm = 8  zs = 3  f = 5
+                               
+
+C:\optimized.png is already optimized.
+
+** Status report
+3 file(s) have been processed.
+1 error(s) have been encountered.
+";
+
+    [Fact]
+    public void The_OutputParser_returns_failure_and_outputs_the_two_errors()
+    {
+        ParseResult result = _outputParser.Parse(new[] { @"C:\optimized.png", @"C:\unoptimized.png", @"C:\file that does not exit.png" }, _output);
+
+        result.Failed.Should().BeTrue();
+
+        result.Output.Count.Should().Be(2);
+
+        result.Output.First().Should().Be(_outputParser.NotOptimizedErrorMessageFormatter(@"C:\unoptimized.png"));
+        result.Output.Last().Should().Be(_outputParser.UnknownErrorMessageFormatter(@"C:\file that does not exit.png", "Can't open the input file"));
     }
 }
